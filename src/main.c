4101d4c6049f188ddb6f3fbc857ed2d0cc6fdd6c
laumy <mingyuan.liu@spacemit.com>
2026-05-08 13:48:01 +0800

    ci: add buildable smoke target

---
+++ src/main.c
@@ -0,0 +1,12 @@
+/*
+ * Copyright (C) 2026 SpacemiT (Hangzhou) Technology Co. Ltd.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+    puts("ci-test: build ok");
+    return 0;
+}
